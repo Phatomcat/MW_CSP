35151ace7ffb08028925ba4c9737ceb549582fde
Phatomcat <marshall.white@ucas-edu.net>
2025-08-29 14:42:44 -0600

    added notes

---
+++ notes/variables.py
@@ -0,0 +1,25 @@
+# MW 7th Variables Notes
+
+#What is one way to have your program give an output?
+# A print statement, it takes anything that is put inbetween the parenthesis and sends it to the terminal
+print(" Welcome to the variable notes!")
+#What is a variable?
+#  a variable is a value that can change
+
+#How do I assign a variable?
+# Give it a name, then use a assignment operator to give it a value (=), then give it a value. NO SPACES use _ and all letters lowercase. can use camel case, second word and beyond start with a capital and get rid of the spaces. Pascal case is when you capitalize the first letter of every word, no spaces. DO NOT MAKE A VARIABLE MORE THAN 3 WORDS
+school= "UCAS"
+singleDigit= 4
+DoubleDigit= 24
+#How do I reassign a variable?
+# Change the content in the variable
+school= 1000000000
+print(school)
+#How do I create an input into my program?
+# to get a input from user write "input" then do () and in the parenthesis ask the user a question.
+name= input("What is your name?")
+
+print("Hello," name)
+
+#How many things can be put into the same print statement?
+#
