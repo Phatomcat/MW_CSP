31aa7742487ed4d36f8fae53d62549345154044d
Phatomcat <marshall.white@ucas-edu.net>
2025-09-04 14:40:41 -0600

    did expression notes

---
+++ notes/expressions_notes.py
@@ -0,0 +1,49 @@
+#MW 7th EXPRESSIONS NOTES
+
+#What is an algorithm?
+
+# A set of instructions to complete a task.
+name = input("What is your name?\n")
+print("Hello,",name)
+# WORD PROBLEM EXAMPLE
+# Treyson has 12 apples he has 5 friends that he wants to give apples to, how many does each friend get
+apples=12
+friends=5
+print("Each friend gets", apples/friends, "apples.")
+
+#List steps in an algorithm
+
+#1. Declare variables. (aka get the info we need.) 2. Preform any equations you have to do. 3. Display the results.
+#Average age group of a group of 4 people
+ages1 = 66
+age2 = 24
+age3 = 43
+age4 = 53
+total_ages = ages1 + age2 + age3 + age4
+average= total_ages/4
+
+print("The average age is of the group is,", average,)
+
+#List ALL of the different mathematical operators (Give me the symbol and tell me what it does)
+num_one=24
+num_two = 3
+
+print("Addition(+) ", num_one+num_two)
+print("Subtracion(-) ", num_one-num_two)
+print("Multiplication(*) ", num_one*num_two)
+print("Division(/) ", num_one/num_two)
+print("Exponents(**) ", num_one**num_two)
+print("Integer Division(//) ", num_one//num_two)
+print("Modulo(%) ", num_one%num_two)
+#A INTAGER DIVISION WILL GIVE YOU A WHOLE NUMBER, NUMBERS WITH DECIMALS ARE CALLED FLOATS. MODULO GIVES YOU THE REMAINDER OF THE DIVISION.
+
+
+#List ALL of the different assignment operators (Give me the symbols and what it does)
+
+#
+
+#Why are expressions so important in programming?
+
+
+
+
