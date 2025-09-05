9cff1f03e4b09e5742d88c43454841ad3a730828
Phatomcat <marshall.white@ucas-edu.net>
2025-09-05 21:23:17 +0000

    DID SOME CODING WITH BUDGET CALCULATOR AND EXPRESSION PRACTICE. NOT FINISHED!!!!!!!!

--- assignments/budget_calculator.py
+++ assignments/budget_calculator.py
@@ -1,6 +1,22 @@
 #MW 7th BUDGET CALCULATOR
 
+print("Hello, this is a budget calculator, below please type your income.")
 income=int(input("How much is your income?\n"))
-rent=
-spend=(utilities=grocieres+transport+saving)
-#percent_rent= rent divided by income times 100 
+
+rent=int(input("Okay, Now how much is your rent?\n"))
+utilities=int(input("How much are your utilites?\n"))
+groceries=int(input("How much are your groceries?\n"))
+transportation=int(input("How much is your transportation?\n"))
+
+
+
+spend=(income-(utilities+groceries+transportation+rent))
+save=round(spend*.1)
+budget=round(income*.99)
+#print(int(rent*.1,"T"))
+
+#percent_rent= rent divided by income times 100
+
+
+print("This is how much you can spend,", budget)
+print("This is how much of your income your should save,", (save))
