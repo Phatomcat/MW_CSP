5998cbb12771f4282573097404856eb18e725c32
Phatomcat <marshall.white@ucas-edu.net>
2025-09-06 02:24:38 +0000

    FINISHED BUDGET CALCULATOR

--- assignments/budget_calculator.py
+++ assignments/budget_calculator.py
@@ -8,15 +8,37 @@ utilities=int(input("How much are your utilites?\n"))
 groceries=int(input("How much are your groceries?\n"))
 transportation=int(input("How much is your transportation?\n"))
 
+spend=(rent+utilities+groceries+transportation)
 
+savings= int(income * .10)
 
-spend=(income-(utilities+groceries+transportation+rent))
-save=round(spend*.1)
-budget=round(income*.99)
-#print(int(rent*.1,"T"))
+budget= int(income-(spend+savings))
 
-#percent_rent= rent divided by income times 100
+rent_percent=round((rent/income)*100)
+utilities_percent=round(utilities/income*100)
+groceries_percent=round((groceries/income*100))
+transportation_percent=round((transportation/income*100))
+savings_percent=round((savings/income*100))
 
+#RENT OUTPUT
+print("Your rent is ",rent)
+print("and that is",rent_percent)
+print("percent of your income!\n")
+#UTILITIES OUTPUT
+print("Your utilities are", utilities)
+print("and that is ", utilities_percent)
+print("percent of your income!\n")
+#GROCERIES OUTPUT
+print("Your groceries are ",groceries)
+print("and that is",groceries_percent)
+print("percent of your income!\n")
+#TRANSPORTATION OUTPUT
+print("Your transportaion is ",transportation)
+print("and that is",transportation_percent)
+print("percent of your income!\n")
 
-print("This is how much you can spend,", budget)
-print("This is how much of your income your should save,", (save))
+print("You should save", savings)
+print("and that is ",savings_percent)
+print("percent of your income!\n")
+
+print("Your budget would be", budget)
