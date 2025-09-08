62bfa3dc6745dc2191e4c259e4e2d0538ad0af5c
Phatomcat <marshall.white@ucas-edu.net>
2025-09-08 14:41:32 -0600

    DID SOME NOTES

--- notes/string_notes.py
+++ notes/string_notes.py
@@ -1,1 +1,45 @@
 # I DID IT!
+#MW 7th STRING NOTES
+
+#What makes something a string?
+# A set of symbols held together by quotation marks.
+firt_name = input('What is your first name:\n').strip().title()
+last_name = input('What is your last name:\n').strip().title()
+s='sksksksksks'
+full_name= firt_name+' '+last_name
+print('welcome to my program:',full_name +'!')
+
+#Why do we have strings?
+#
+
+#How do stupid proofing and sanitization relate to each other?
+#
+
+#What is debugging?
+#
+
+#How do you debug the different types of errors?
+#
+
+#Describe what each of the methods below does:
+
+
+#find()
+
+#
+
+#concatenate (add)
+
+#puts 2 stings together
+
+#upper()
+
+#
+
+#lower()
+
+#
+
+#strip()
+
+# strips the spaces at the start and end
