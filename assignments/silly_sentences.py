ea49292271d8ca11ded87dfdecbeb8531ab2c22d
Phatomcat <marshall.white@ucas-edu.net>
2025-09-10 21:08:44 +0000

    FINISHED ASSIGNMENTS

--- assignments/silly_sentences.py
+++ assignments/silly_sentences.py
@@ -5,10 +5,10 @@ second_word = input('Give me a person\n')
 third_word = input('Give me a direction\n')
 
 print('Twinkle, twinkle, little '+first_word)
-print('How I wonder what '+second_word+'are.')
-print(third_word+'above the world so high')
-print('Like a '+first_word+'in the sky')
-print('Twinkle, twinkle, little'+first_word)
-print('How I wonder what'+second_word)
+print('How I wonder what '+second_word+' '+'are.')
+print(third_word+' '+'above the world so high')
+print('Like a '+first_word+' ''in the sky')
+print('Twinkle, twinkle, little '+first_word)
+print('How I wonder what '+second_word+' '+'are.')
 
 
