2cf75b59e6fbfc56be166db56a506d6e0ba8794f
Phatomcat <marshall.white@ucas-edu.net>
2025-09-16 23:25:07 +0000

    FINISHED UPDATE HELLO, UPDATE CALCULATOR

--- assignments/update_calculator.py
+++ assignments/update_calculator.py
@@ -1,8 +1,25 @@
 #MW 7th UPDATE CALCULATOR
 
-def get_num():
-    income=get_num("income")
-    rent=get_num("rent")
-    return round(rent/income*100,2)
+def get_num(question):
+    return int(input("How much is your "+question+"?\n"))
 
+def percent(income, expense):
+    return round((expense / income)*100,2)
 
+print("Welcome to the budget calculator")
+
+income = get_num("income")
+rent=get_num("rent")
+utilities=get_num("utilities")
+groceries=get_num("groceries")
+transportation=get_num("transportation")
+
+rent_percent= percent(income,rent)
+utilities_percent=percent(income,utilities)
+groceries_percent=percent(income,groceries)
+transportation_percent=percent(income,transportation)
+
+print(f"Your rent is {rent} and that is {rent_percent} percent of your income.")
+print(f"Your utilites are {utilities} and that is {utilities_percent} precent of your income.")
+print(f"Your groceries are {groceries} and that is {groceries_percent} percent of your income.")
+print(f"Your transportation is {transportation} and that is {transportation_percent} percent of your income.")
--- assignments/update_hello.py
+++ assignments/update_hello.py
@@ -1,15 +1,15 @@
 #MW 7th UPDATE HELLO
 
 def hello(name):
-    print(f"Hello {name}")
-    print(f"Hello {name}")
-    print(f"Hello {name}")
-    print(f"Hello {name}")
-    print(f"Hello {name}")
+    print("Hello, ",name)
+hello("Bob")
+hello("Josh")
+hello("Colin")
+hello("marshall")
+hello("Brooks")
 
 
-name=input("What is your name\n")
-hello(name)
+
 
 
 
