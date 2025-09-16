4726a0750ed4cc0efdbb42ddecf9c70f1d0756f3
Phatomcat <marshall.white@ucas-edu.net>
2025-09-16 14:39:01 -0600

    ADDED FILES

---
+++ assignments/update_calculator.py
@@ -0,0 +1,8 @@
+#MW 7th UPDATE CALCULATOR
+
+def get_num():
+    income=get_num("income")
+    rent=get_num("rent")
+    return round(rent/income*100,2)
+
+
---
+++ assignments/update_hello.py
@@ -0,0 +1,18 @@
+#MW 7th UPDATE HELLO
+
+def hello(name):
+    print(f"Hello {name}")
+    print(f"Hello {name}")
+    print(f"Hello {name}")
+    print(f"Hello {name}")
+    print(f"Hello {name}")
+
+
+name=input("What is your name\n")
+hello(name)
+
+
+
+
+
+
