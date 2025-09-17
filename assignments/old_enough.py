f979d4f9570de635b236b134a69a0356eb540cd5
Phatomcat <marshall.white@ucas-edu.net>
2025-09-17 21:15:54 +0000

    DID OLD ENOUGH ASSIGNMENT, ADDED TIME FILE.

---
+++ assignments/old_enough.py
@@ -0,0 +1,8 @@
+#MW 7th OLD ENOUGH ASSIGNMENT
+
+how_old = int(input("How old are you?\n"))
+
+if how_old >= 15:
+    print(f"Since you are {how_old}, you can get a learners permit!")
+elif how_old:
+    print(f"Since you are {how_old}, you can not get a learners permit.")
