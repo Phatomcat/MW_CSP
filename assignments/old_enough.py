193360cd6d331936b17ace3443676ca4c47ccced
Phatomcat <marshall.white@ucas-edu.net>
2025-09-18 18:36:24 +0000

    FINISHED OLD ENOUGH ASSIGNMENT

--- assignments/old_enough.py
+++ assignments/old_enough.py
@@ -2,7 +2,7 @@
 
 how_old = int(input("How old are you?\n"))
 
-if how_old >= 15:
-    print(f"Since you are {how_old}, you can get a learners permit!")
-elif how_old:
+if how_old < 15:
     print(f"Since you are {how_old}, you can not get a learners permit.")
+elif how_old:
+    print(f"Since you are {how_old}, you can get a learners permit!")
