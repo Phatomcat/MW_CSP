ece7048d410f2f7fe52216ef1c5caf976b18584b
Phatomcat <marshall.white@ucas-edu.net>
2025-09-23 14:24:30 -0600

    FINISHED FAMILY LOOP AND FIZZ BUZZ

---
+++ assignments/family_loop.py
@@ -0,0 +1,6 @@
+#MW 7th FAMILY LOOP
+
+members = ["Alton","Kimball","Marinda","Michael"]
+
+for member in members:
+    print(f"Hello, {member}")
---
+++ assignments/fizz_buzz.py
@@ -0,0 +1,16 @@
+#MW 7th FIZZ BUZZ
+x=1
+
+
+while x <51:
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print ("Fizz")
+    elif x % 5 ==0:
+        print("Buzz")
+    else:
+        print(x)
+    x+=1
+
+
