4a8f895818a9c804cb4eb97652cb87da506ee3c9
Phatomcat <marshall.white@ucas-edu.net>
2025-10-10 14:41:25 -0600

    ALMOST DONE WITH TIME OF DAY

--- c_programming/practices/old_enough.c
+++ c_programming/practices/old_enough.c
@@ -24,6 +24,7 @@ if (age >18){
 
 
 
+
     return 0;
 }
 
--- c_programming/practices/time_of_day.c
+++ c_programming/practices/time_of_day.c
@@ -4,8 +4,17 @@
 
 int main (void){
 
-
-
+int time;
+printf("What time is it in Military time?\n");
+scanf("%d\n", &time);
+if (time >=0 && time >11){
+    printf("Good morning!");
+}else if (time >=11 && time < 18){
+    printf("Good afternoon!");
+}else if (time >= 18 && time <=24){
+    printf("Good night!");
+}else (time > 24);
+    printf("Thats not a time!");
 
 
     return 0;
