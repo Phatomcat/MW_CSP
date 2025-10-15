1f0ea7838f922740996a1d8824b5c3f01b1406dd
Phatomcat <marshall.white@ucas-edu.net>
2025-10-15 02:31:42 +0000

    STARTED ON ASSIGNMENTS

---
+++ c_programming/practices/update_helloworld.c
@@ -0,0 +1,19 @@
+// MW 7th UPDATE HELLO WORLD
+
+
+#include <stdio.h>
+#include <string.h>
+void names(char*name){
+    printf("Hello %s",names);
+}
+
+
+
+int main(void){
+names("Marshall\n");
+names("kimball\n");
+names("Alton\n");
+
+
+    return 0;
+}
